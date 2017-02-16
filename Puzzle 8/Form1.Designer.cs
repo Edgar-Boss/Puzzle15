6162be83226792e92413bd0fec0e0e9535aff6c5
Edgar-Boss <edgardiazortiz@gmail.com>
2017-02-16 02:18:03 -0600

    primera entrada 16/02/2017
    modificacion de  puzzle 8

---
+++ Puzzle 8/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Puzzle_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.stak = new System.Windows.Forms.Panel();
+            this.I02 = new System.Windows.Forms.TextBox();
+            this.I10 = new System.Windows.Forms.TextBox();
+            this.I01 = new System.Windows.Forms.TextBox();
+            this.I11 = new System.Windows.Forms.TextBox();
+            this.I12 = new System.Windows.Forms.TextBox();
+            this.I20 = new System.Windows.Forms.TextBox();
+            this.I21 = new System.Windows.Forms.TextBox();
+            this.I00 = new System.Windows.Forms.TextBox();
+            this.I22 = new System.Windows.Forms.TextBox();
+            this.I03 = new System.Windows.Forms.TextBox();
+            this.I13 = new System.Windows.Forms.TextBox();
+            this.I23 = new System.Windows.Forms.TextBox();
+            this.I30 = new System.Windows.Forms.TextBox();
+            this.I31 = new System.Windows.Forms.TextBox();
+            this.I33 = new System.Windows.Forms.TextBox();
+            this.I32 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // stak
+            // 
+            this.stak.AutoScroll = true;
+            this.stak.Location = new System.Drawing.Point(344, 27);
+            this.stak.Name = "stak";
+            this.stak.Size = new System.Drawing.Size(441, 605);
+            this.stak.TabIndex = 2;
+            this.stak.Paint += new System.Windows.Forms.PaintEventHandler(this.stak_Paint);
+            // 
+            // I02
+            // 
+            this.I02.Location = new System.Drawing.Point(149, 68);
+            this.I02.Name = "I02";
+            this.I02.Size = new System.Drawing.Size(20, 20);
+            this.I02.TabIndex = 14;
+            this.I02.Text = "3";
+            // 
+            // I10
+            // 
+            this.I10.Location = new System.Drawing.Point(100, 94);
+            this.I10.Name = "I10";
+            this.I10.Size = new System.Drawing.Size(20, 20);
+            this.I10.TabIndex = 15;
+            this.I10.Text = "1";
+            // 
+            // I01
+            // 
+            this.I01.Location = new System.Drawing.Point(126, 68);
+            this.I01.Name = "I01";
+            this.I01.Size = new System.Drawing.Size(20, 20);
+            this.I01.TabIndex = 13;
+            this.I01.Text = "2";
+            // 
+            // I11
+            // 
+            this.I11.Location = new System.Drawing.Point(126, 94);
+            this.I11.Name = "I11";
+            this.I11.Size = new System.Drawing.Size(20, 20);
+            this.I11.TabIndex = 16;
+            this.I11.Text = "6";
+            // 
+            // I12
+            // 
+            this.I12.Location = new System.Drawing.Point(149, 94);
+            this.I12.Name = "I12";
+            this.I12.Size = new System.Drawing.Size(20, 20);
+            this.I12.TabIndex = 17;
+            this.I12.Text = "7";
+            // 
+            // I20
+            // 
+            this.I20.Location = new System.Drawing.Point(100, 120);
+            this.I20.Name = "I20";
+            this.I20.Size = new System.Drawing.Size(20, 20);
+            this.I20.TabIndex = 18;
+            this.I20.Text = "5";
+            // 
+            // I21
+            // 
+            this.I21.Location = new System.Drawing.Point(126, 120);
+            this.I21.Name = "I21";
+            this.I21.Size = new System.Drawing.Size(20, 20);
+            this.I21.TabIndex = 19;
+            this.I21.Text = "10";
+            // 
+            // I00
+            // 
+            this.I00.Location = new System.Drawing.Point(100, 68);
+            this.I00.Name = "I00";
+            this.I00.Size = new System.Drawing.Size(20, 20);
+            this.I00.TabIndex = 12;
+            this.I00.Text = "0";
+            // 
+            // I22
+            // 
+            this.I22.Location = new System.Drawing.Point(152, 120);
+            this.I22.Name = "I22";
+            this.I22.Size = new System.Drawing.Size(20, 20);
+            this.I22.TabIndex = 20;
+            this.I22.Text = "11";
+            // 
+            // I03
+            // 
+            this.I03.Location = new System.Drawing.Point(175, 68);
+            this.I03.Name = "I03";
+            this.I03.Size = new System.Drawing.Size(20, 20);
+            this.I03.TabIndex = 21;
+            this.I03.Text = "4";
+            // 
+            // I13
+            // 
+            this.I13.Location = new System.Drawing.Point(175, 94);
+            this.I13.Name = "I13";
+            this.I13.Size = new System.Drawing.Size(20, 20);
+            this.I13.TabIndex = 22;
+            this.I13.Text = "8";
+            // 
+            // I23
+            // 
+            this.I23.Location = new System.Drawing.Point(178, 120);
+            this.I23.Name = "I23";
+            this.I23.Size = new System.Drawing.Size(20, 20);
+            this.I23.TabIndex = 23;
+            this.I23.Text = "12";
+            // 
+            // I30
+            // 
+            this.I30.Location = new System.Drawing.Point(100, 146);
+            this.I30.Name = "I30";
+            this.I30.Size = new System.Drawing.Size(20, 20);
+            this.I30.TabIndex = 24;
+            this.I30.Text = "9";
+            // 
+            // I31
+            // 
+            this.I31.Location = new System.Drawing.Point(126, 146);
+            this.I31.Name = "I31";
+            this.I31.Size = new System.Drawing.Size(20, 20);
+            this.I31.TabIndex = 25;
+            this.I31.Text = "13";
+            // 
+            // I33
+            // 
+            this.I33.Location = new System.Drawing.Point(178, 146);
+            this.I33.Name = "I33";
+            this.I33.Size = new System.Drawing.Size(20, 20);
+            this.I33.TabIndex = 26;
+            this.I33.Text = "15";
+            // 
+            // I32
+            // 
+            this.I32.Location = new System.Drawing.Point(152, 146);
+            this.I32.Name = "I32";
+            this.I32.Size = new System.Drawing.Size(20, 20);
+            this.I32.TabIndex = 27;
+            this.I32.Text = "14";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 172);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 172);
+            this.textBox1.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 401);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.I32);
+            this.Controls.Add(this.I33);
+            this.Controls.Add(this.I31);
+            this.Controls.Add(this.I30);
+            this.Controls.Add(this.I23);
+            this.Controls.Add(this.I13);
+            this.Controls.Add(this.I03);
+            this.Controls.Add(this.I22);
+            this.Controls.Add(this.I00);
+            this.Controls.Add(this.I21);
+            this.Controls.Add(this.stak);
+            this.Controls.Add(this.I20);
+            this.Controls.Add(this.I12);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.I11);
+            this.Controls.Add(this.I01);
+            this.Controls.Add(this.I10);
+            this.Controls.Add(this.I02);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel stak;
+        private System.Windows.Forms.TextBox I02;
+        private System.Windows.Forms.TextBox I10;
+        private System.Windows.Forms.TextBox I01;
+        private System.Windows.Forms.TextBox I11;
+        private System.Windows.Forms.TextBox I12;
+        private System.Windows.Forms.TextBox I20;
+        private System.Windows.Forms.TextBox I21;
+        private System.Windows.Forms.TextBox I00;
+        private System.Windows.Forms.TextBox I22;
+        private System.Windows.Forms.TextBox I03;
+        private System.Windows.Forms.TextBox I13;
+        private System.Windows.Forms.TextBox I23;
+        private System.Windows.Forms.TextBox I30;
+        private System.Windows.Forms.TextBox I31;
+        private System.Windows.Forms.TextBox I33;
+        private System.Windows.Forms.TextBox I32;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Puzzle 8/Form1.cs
@@ -0,0 +1,561 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int[,] completo = new int[4, 4];
+
+        int lv = 200;
+        int lh = 50;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int[,,] tablero = new int[200, 4, 4];
+            int[,,] candidato = new int[10, 4, 4];
+            int indtab = 0, indcad = 0;
+            int[,,] visitados = new int[500, 4, 4];
+            int indvis = 0;
+            int H = 0, haux = 0;//marca errores
+            int index = -1;
+            string cad = "";
+            bool rep = false;
+            TextBox[] V = new TextBox[2000];//textbox donde se mostraran las vertices
+
+            stak.Controls.Clear();
+            //inicializar valores de tablero inicial 
+            visitados[0, 0, 0] = tablero[indtab, 0, 0] = int.Parse(I00.Text);
+            visitados[0, 0, 1] = tablero[indtab, 0, 1] = int.Parse(I01.Text);
+            visitados[0, 0, 2] = tablero[indtab, 0, 2] = int.Parse(I02.Text);
+            visitados[0, 0, 3] = tablero[indtab, 0, 3] = int.Parse(I03.Text);
+            visitados[0, 1, 0] = tablero[indtab, 1, 0] = int.Parse(I10.Text);
+            visitados[0, 1, 1] = tablero[indtab, 1, 1] = int.Parse(I11.Text);
+            visitados[0, 1, 2] = tablero[indtab, 1, 2] = int.Parse(I12.Text);
+            visitados[0, 1, 3] = tablero[indtab, 1, 3] = int.Parse(I13.Text);
+            visitados[0, 2, 0] = tablero[indtab, 2, 0] = int.Parse(I20.Text);
+            visitados[0, 2, 1] = tablero[indtab, 2, 1] = int.Parse(I21.Text);
+            visitados[0, 2, 2] = tablero[indtab, 2, 2] = int.Parse(I22.Text);
+            visitados[0, 2, 3] = tablero[indtab, 2, 3] = int.Parse(I23.Text);
+            visitados[0, 3, 0] = tablero[indtab, 3, 0] = int.Parse(I30.Text);
+            visitados[0, 3, 1] = tablero[indtab, 3, 1] = int.Parse(I31.Text);
+            visitados[0, 3, 2] = tablero[indtab, 3, 2] = int.Parse(I32.Text);
+            visitados[0, 3, 3] = tablero[indtab, 3, 3] = int.Parse(I33.Text);
+            indvis++;
+            //inicializar valores de tablero completo
+
+            int contador = 1;
+            for (int k = 0; k < 4; k++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    completo[k, j] = contador;
+                    contador++;
+                }
+                cad += Environment.NewLine;
+            }
+            completo[3, 3] = 0;
+
+            index = -1;
+            bool mverror = false;
+            int contv = 0;
+           
+            while (H < 15)
+            //for (int t = 0; t < 742; t++)
+            {
+                for (int r = 0; r <= indtab; r++)
+                {
+
+                  
+                    H = 0;
+                    for (int k = 0; k < 2; k++)
+                    {//buscar en x
+                        haux = 0;
+
+                        try
+                        {
+
+
+                            cad = "";
+                            for (int t = 0; t < 4; t++)
+                            {
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    cad += tablero[indtab, t, j].ToString();
+                                    cad += ",";
+                                }
+                                cad += Environment.NewLine;
+                            }
+                            cad += Environment.NewLine;
+                            mverror = mover_x(tablero, index, indtab);//mueve en eje 
+                            
+                            for (int t = 0;t < 4; t++)
+                            {
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    cad += tablero[indtab,t, j].ToString();
+                                    cad += ",";
+                                }
+                                cad += Environment.NewLine;
+                            }
+                            textBox1.Text = cad;
+                            
+                            rep = Busca_en_vistados(tablero, visitados, indvis, indtab);//busca que no este repetido
+
+                            if (rep == false)
+                            {
+                              
+                                try
+                                {
+                                    Copia_vist(visitados, tablero, indvis, indtab);//guarda a visitados
+                                    indvis++;
+                                    haux = Compara_competo(tablero, indtab);//busca cuantos aciertos tiene el nuevo movimiento
+                                   
+
+                                    if (haux > H)//si es mayor al aterior entra
+                                    {
+                                        indcad = 0;
+                                        Copia_matriz(candidato, tablero, indtab, indcad);//copia el nuevo movimiento a candidato 
+                                        H = haux;// guarda en H para seguir comparando
+
+                                    }
+                                    else if (haux == H)
+                                    {
+                                        indcad++;
+                                        Copia_matriz(candidato, tablero, indtab, indcad);
+
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.ToString());
+
+                                }
+
+
+
+                            }
+
+
+
+
+                        }
+                        catch (Exception )
+                        {
+                            mverror = true;
+                        }
+
+                        index = index * -1;
+
+
+
+
+                        if (mverror == false)
+                        {
+
+                           mover_x(tablero, index, indtab);
+
+                        }
+
+
+                        mverror = false;
+
+                    }
+
+
+                    for (int k = 0; k < 2; k++)
+                    {//buscar en y
+                        haux = 0;
+
+                        try
+                        {
+                            cad = "";
+                            for (int t = 0; t < 4; t++)
+                            {
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    cad += tablero[indtab, t, j].ToString();
+                                    cad += ",";
+                                }
+                                cad += Environment.NewLine;
+                            }
+                            cad += Environment.NewLine;
+
+                            mverror = mover_y(tablero, index, indtab);//mueve en eje y
+                            
+                            for (int t = 0; t < 4; t++)
+                            {
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    cad += tablero[indtab, t, j].ToString();
+                                    cad += ",";
+                                }
+                                cad += Environment.NewLine;
+                            }
+                            textBox1.Text = cad;
+                            rep = Busca_en_vistados(tablero, visitados, indvis, indtab);
+
+
+                            if (rep == false)
+                            {
+                                try
+                                {
+                                    Copia_vist(visitados, tablero, indvis, indtab);//guarda a visitados
+                                    indvis++;
+                                    haux = Compara_competo(tablero, indtab);
+                                    
+                                    if (haux > H)
+                                    {
+                                        indcad = 0;
+                                        Copia_matriz(candidato, tablero, indtab, indcad);
+                                        H = haux;
+                                    }
+                                    else if (haux == H)
+                                    {
+                                        indcad++;
+                                        Copia_matriz(candidato, tablero, indtab, indcad);
+
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.ToString());
+                                }
+
+                            }
+
+
+
+                        }
+                        catch (Exception es)
+                        {
+                            mverror = true;
+                        }
+
+
+
+                        index = index * -1;
+
+                        if (mverror == false)
+                            mover_y(tablero, index, indtab);
+
+                        mverror = false;
+
+
+
+                    }
+
+
+                }
+
+                lv = 200;
+
+                for (int m = 0; m < 4; m++)
+                {
+
+                    lv = 200;
+                    for (int j = 0; j < 4; j++)
+                    {
+
+                        
+
+                        V[contv] = new TextBox();
+
+                        V[contv].Text = tablero[indtab, m,j].ToString();
+                        V[contv].Size = new Size(20, 20);
+                        V[contv].Location = new Point(lv, lh);
+                        V[contv].BackColor = Color.AliceBlue;
+                        Controls.Add(V[contv]);
+                        stak.Controls.Add(V[contv]);
+                        lv += 20;
+                        contv++;
+
+
+
+
+
+
+                        Pen myPe;
+                        myPe = new Pen(Color.Black);
+                        Graphics formGraphic;
+                        formGraphic = stak.CreateGraphics();
+                        stak.CreateGraphics().Save();
+                        formGraphic.DrawLine(myPe, 230, lh, 230, lh+90);
+                        myPe.Dispose();
+                        formGraphic.Dispose();
+
+
+
+
+
+
+
+                    }
+
+                    lh += 20;
+                   
+                }
+
+                
+                V[contv] = new TextBox();
+
+                V[contv].Text = (H-1).ToString();
+                V[contv].Size = new Size(20, 20);
+                V[contv].Location = new Point(lv, lh);
+                
+                V[contv].BackColor = Color.AliceBlue;
+                Controls.Add(V[contv]);
+                stak.Controls.Add(V[contv]);
+                lv += 20;
+                contv++;
+
+                lh += 70;
+
+                indtab = indcad;
+                cand_tab(candidato, indcad, tablero, indtab);
+
+
+
+
+
+
+
+              
+            }
+
+
+
+
+
+            for (int m = 0; m < 4; m++)
+            {
+                lv = 200;
+                for (int j = 0; j < 4; j++)
+                {
+
+
+
+                    V[contv] = new TextBox();
+
+                    V[contv].Text = tablero[indtab, m, j].ToString();
+                    V[contv].Size = new Size(20, 20);
+                    V[contv].Location = new Point(lv, lh);
+                    V[contv].BackColor = Color.AliceBlue;
+                    Controls.Add(V[contv]);
+                    stak.Controls.Add(V[contv]);
+                    lv += 20;
+                    contv++;
+
+
+
+
+                }
+
+                lh += 20;
+            }
+
+
+            
+
+
+
+        }
+
+
+        public void cand_tab(int[,,] cand, int indcand, int[,,] tablero, int indtab)
+        {
+            for (int j = 0; j <= indcand; j++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int l = 0; l < 4; l++)
+                    {
+                        tablero[j, k, l] = cand[j, k, l];
+                    }
+
+                }
+            }
+
+        }
+        public bool mover_x(int[,,] tablero, int index, int indtab)
+        {
+            bool mverror=false;
+            int[,] pos0 = new int[1, 2];
+
+               
+
+            try
+            {
+                busca_pos0(tablero, pos0, indtab);
+
+                tablero[indtab, pos0[0, 0], pos0[0, 1]] = tablero[indtab, pos0[0, 0] + index, pos0[0, 1]];
+                tablero[indtab, pos0[0, 0] + index, pos0[0, 1]] = 0;
+            }
+            catch (Exception)
+            {
+                mverror = true;
+            }
+            return mverror;
+        }
+
+
+        public bool mover_y(int[,,] tablero, int index, int indtab)
+        {
+
+            int[,] pos0 = new int[1, 2];
+            bool mverror = false;
+
+            try
+            {
+                busca_pos0(tablero, pos0, indtab);
+
+                tablero[indtab, pos0[0, 0], pos0[0, 1]] = tablero[indtab, pos0[0, 0], pos0[0, 1] + index];
+                tablero[indtab, pos0[0, 0], pos0[0, 1] + index] = 0;
+            }
+            catch (Exception)
+            {
+                mverror = true;
+            }
+
+            return mverror;
+        }
+
+        public void busca_pos0(int[,,] tablero, int[,] pos0, int indtab)
+        {
+
+            for (int k = 0; k < 4; k++)//buscar 0 (espacio)
+            {
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    if (tablero[indtab, k, j] == 0)
+                    {
+                        pos0[0, 0] = k;
+                        pos0[0, 1] = j;
+                    }
+
+                }
+
+
+            }
+
+        }
+
+        public int Compara_competo(int[,,] tablero, int indtab)
+        {
+            int haux = 0;
+
+            for (int k = 0; k < 4; k++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    if (tablero[indtab, k, j] == completo[k, j])
+                        haux++;
+
+
+
+                }
+            }
+
+            return haux;
+        }
+
+        public void Copia_matriz(int[,,] candidato, int[,,] tablero, int indtab, int indcand)
+        {
+
+
+
+            for (int k = 0; k < 4; k++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    candidato[indcand, k, j] = tablero[indtab, k, j];
+
+                }
+            }
+        }
+
+
+        public void Copia_vist(int[,,] visitados, int[,,] tablero, int index, int indtab)
+        {
+
+            for (int k = 0; k < 4; k++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    visitados[index, k, j] = tablero[indtab, k, j];
+
+
+
+                }
+            }
+        }
+
+        public bool Busca_en_vistados(int[,,] Tablero, int[,,] visitados, int indvisit, int indtab)
+        {
+
+            int contador = 0;
+            bool rep = false;
+            for (int k = 0; k <= indvisit; k++)
+            {
+                contador = 0;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int l = 0; l < 4; l++)
+                    {
+
+
+                        if (Tablero[indtab, j, l] == visitados[k, j, l])
+                        {
+
+                            contador++;
+                        }
+                    }
+                }
+
+                if (contador >= 15)
+                {
+                    rep = true;
+                    break;
+                }
+
+
+
+            }
+
+            return rep;
+        }
+
+        private void stak_Paint(object sender, PaintEventArgs e)
+        {
+
+            Pen myPen;
+            myPen = new Pen(Color.Black);
+            Graphics formGraphics;
+            formGraphics = stak.CreateGraphics();
+            stak.CreateGraphics().Save();
+            formGraphics.DrawLine(myPen, 230, 50, 230, lh );
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+    }
+}
